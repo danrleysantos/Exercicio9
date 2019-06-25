ece956c31c4d1600a102cf71a9c34222dfa531e9
ifnmg <ifnmg@192.168.163.14>
2019-06-24 21:27:17 -0300

    exercicio9

---
+++ src/arara.java
@@ -0,0 +1,13 @@
+
+public class arara extends ave {
+    
+    public arara(String n, boolean v){
+    
+        super(n, v);
+        
+    }
+    public void talk(){
+    
+        System.out.println("Arara, Arara");
+    }
+}
